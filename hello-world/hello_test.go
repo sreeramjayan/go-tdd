83aea12f12989a7a4da312539e1a24a04b347574
Sreeram Jayan <sreeramjayan@gmail.com>
2022-08-21 09:46:21 -0500

    Add tests and refactor code

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,9 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Sreeram")
-	expected := "Hello, Sreeram!!"
+	t.Run("saying Hello to people", func(t *testing.T) {
+		got := Hello("Sreeram")
+		expected := "Hello, Sreeram!!"
 
+		assertCorrectMessage(t, got, expected)
+	})
+
+	t.Run("saying Hello, World!! when an empty string is provided", func(t *testing.T) {
+		got := Hello("")
+		expected := "Hello, World!!"
+
+		assertCorrectMessage(t, got, expected)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, expected string) {
+	t.Helper()
 	if got != expected {
 		t.Errorf("got %q expected %q", got, expected)
 	}
